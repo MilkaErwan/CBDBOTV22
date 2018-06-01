846a3c2903cb7f8f7f7603132e7c327f761285f0
MilkaErwan <39467134+MilkaErwan@users.noreply.github.com>
2018-06-01 12:43:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js") 
 const bot = new Discord.Client()
 
-bot.login('NDUxODY5MDIzNzAxODI3NTk0.DfIImA.TRYQJvwik7OYKF_oHE10dTEAxqY')
+bot.login('process.env.TOKEN')
 
 bot.on('message', function (message) {
     if (message.content === '^livrasons' ) {
